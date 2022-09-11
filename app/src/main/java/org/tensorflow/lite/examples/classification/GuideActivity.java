a6de57e76ca31ffa189c398316ddc8bde1d29abc
Lorenzo <lorenzo.massa@stud.unifi.it>
2022-09-11 17:28:39 +0200

    Guide layout

--- app/src/main/java/org/tensorflow/lite/examples/classification/GuideActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/GuideActivity.java
@@ -5,11 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,12 +30,16 @@ public class GuideActivity extends AppCompatActivity {
 
     String text;
     Bitmap img;
-    String audio;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+
+        ScrollView mainScrollView = (ScrollView)findViewById(R.id.idScrollView);
+        mainScrollView.fullScroll(ScrollView.FOCUS_UP);
+        mainScrollView.smoothScrollTo(0,0);
 
         String monumentId = getIntent().getStringExtra("monument_id");
         String language = getIntent().getStringExtra("language");
@@ -37,6 +49,29 @@ public class GuideActivity extends AppCompatActivity {
         loadGuidefromFile("guides/"+monumentId+"/testo.txt");
         loadImageGuide("guides/"+monumentId+"/img.jpg");
 
+        String path = "android.resource://" + getPackageName() + "/";
+
+        switch (monumentId){
+            case "Cattedrale Duomo":
+                path += R.raw.duomo;
+                break;
+            case "Campanile Giotto":
+                path += R.raw.duomo;
+                break;
+            case "Battistero SanGiovanni":
+                path += R.raw.duomo;
+                break;
+            case "Loggia Bigallo":
+                path += R.raw.duomo;
+                break;
+            case "Palazzo Vecchio":
+                path += R.raw.duomo;
+                break;
+            default:
+                break;
+        }
+
+
         TextView titleTextView = findViewById(R.id.titleGuide);
         titleTextView.setText(monumentId);
 
@@ -45,6 +80,25 @@ public class GuideActivity extends AppCompatActivity {
 
         ImageView imageImageView = findViewById(R.id.imgGuide);
         imageImageView.setImageBitmap(img);
+
+        VideoView videoView = findViewById(R.id.videoView);
+        Uri uri = Uri.parse(path);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        mediaController.setMediaPlayer(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.seekTo(1);
+        //videoView.start();
+
+        Button playBtn = findViewById(R.id.idBtnPlay);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calling method to play audio.
+                playAudio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
+            }
+        });
     }
 
     private void loadGuidefromFile(String fileName){
@@ -86,5 +140,36 @@ public class GuideActivity extends AppCompatActivity {
         }
 
         img = BitmapFactory.decodeStream(is);
+    }
+
+    private void playAudio(String audioUrl) {
+
+        if (mediaPlayer == null){
+            mediaPlayer = new MediaPlayer();
+            //mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            try {
+                mediaPlayer.setDataSource(audioUrl);
+                mediaPlayer.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            Toast.makeText(GuideActivity.this, "Audio has been paused", Toast.LENGTH_SHORT).show();
+        } else {
+            mediaPlayer.start();
+            Toast.makeText(GuideActivity.this, "Audio started playing..", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void stopAudio(){
+        mediaPlayer.stop();
+        mediaPlayer.reset();
+        mediaPlayer.release();
+        mediaPlayer=null;
     }
 }
